0997a499ec4a35f141c83cc2943fd31ffa0639bd
Mahan Jafari <mahanjafari14@gmail.com>
2022-01-09 20:05:17 +0330

    bug fixed ishalah

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -6,22 +6,20 @@ import styles from "./Header.module.scss";
 const Header = () => {
   return (
     <header className="navbar mt-md-3" id="home">
-      <div className="container-fluid">
-        <div className="col-12 col-md-2 d-none d-md-flex justify-content-center">
-          <Logo />
-        </div>
+      <div className="col-12 col-md-2 d-none d-md-flex justify-content-center">
+        <Logo />
+      </div>
 
-        <div className={`col-12 col-md-8`}>
-          <Navbar />
-        </div>
+      <div className={`col-12 col-md-8`}>
+        <Navbar />
+      </div>
 
-        <div className="col-12 d-none col-md-2 d-md-flex justify-content-center">
-          <ContactButton>
-            <a className={styles.decor} href="#contactme">
-              Contact Me
-            </a>
-          </ContactButton>
-        </div>
+      <div className="col-12 d-none col-md-2 d-md-flex justify-content-center">
+        <ContactButton>
+          <a className={styles.decor} href="#contactme">
+            Contact Me
+          </a>
+        </ContactButton>
       </div>
     </header>
   );
